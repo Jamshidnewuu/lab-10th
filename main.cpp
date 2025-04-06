fa36e5e629d37d5ce687e1085adda807272142f8
Jamshidnewuu <j.xudoyberdiyev@newuu.uz>
2025-04-06 22:50:29 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,59 +3,51 @@
 #include <ctime>
 using namespace std;
 //problem 1
-/*int main() {
-    int n, sum = 0;
-    cin >> n;
-    int arr[n];
-
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+/*void doMath (int arr [], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
         sum += arr[i];
     }
-
-    cout << "sum=" << sum;
+    cout << sum;
+}
+int main () {
+    int arr[5] = {8, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    doMath(arr, size);
     return 0;
 }*/
 
 //problem 2
-/*double findAverage(int arr[], int size) {
-    double sum = 0;
-    for (int i = 0; i < size; i++)
+/*void doMath (int arr [], int size) {
+    float sum = 0;
+    for (int i = 0; i < size; i++) {
         sum += arr[i];
-    return sum / size;
+    }
+    float avg = sum / size;
+    cout << avg;
 }
-
-int main() {
-    int n;
-    cin >> n;
-    int arr[n];
-
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-
-    cout << "Output: " << findAverage(arr, n);
-    return 0;
+int main () {
+    int arr[4] = {2, 3, 3, 3};
+    int size = sizeof(arr) / sizeof arr[0];
+    doMath(arr, size);
 }*/
 
 //problem 3
-/*int findMin(int arr[], int size) {
-    int minVal = arr[0];
-    for (int i = 1; i < size; i++)
-        if (arr[i] < minVal)
-            minVal = arr[i];
-    return minVal;
+/*void Print (int arr [], int size) {
+    int min = arr[0];
+    int i = 1;
+    while (i < size) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        i++;
+    }
+    cout << "Minimum is: " << min;
 }
-
-int main() {
-    int n;
-    cin >> n;
-    int arr[n];
-
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-
-    cout << "Min=" << findMin(arr, n);
-    return 0;
+int main () {
+    int arr[4] = {8, 3, 1, 3};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    Print(arr, size);
 }*/
 
 //problem 4
@@ -67,12 +59,10 @@ int main() {
             maxIndex = i;
     return maxIndex;
 }
-
 int main() {
     double arr[15];
     for (int i = 0; i < 15; i++)
         cin >> arr[i];
-
     cout << "Largest Index: " << indexOfLargestElement(arr, 15);
     return 0;
 }*/
@@ -84,116 +74,115 @@ int main() {
             return false;
     return true;
 }
-
 int main() {
     int n;
     cin >> n;
     int list1[n], list2[n];
-
     for (int i = 0; i < n; i++) cin >> list1[i];
     for (int i = 0; i < n; i++) cin >> list2[i];
-
-    if (strictlyEqual(list1, list2, n))
+    if (strictlyEqual(list1, list2, n)) {
         cout << "Two lists are strictly identical";
-    else
+    } else {
         cout << "Two lists are not strictly identical";
+        }
     return 0;
 }*/
 
 
 //problem 6
 /*int main() {
-    int freq[26] = {0};
     char ch;
-
-    while (cin >> ch && ch != '0') {
-        if (ch >= 'a' && ch <= 'z')
-            freq[ch - 'a']++;
+    int arr [26];
+    for (int i = 0; i < 26; i++) {
+        arr [i] = 0;
     }
-
-    for (int i = 0; i < 26; i++)
-        if (freq[i] > 0)
-            cout << char('a' + i) << ": " << freq[i] << endl;
-
+    cout << "Enter characters: ";
+    while (true) {
+        cin >> ch;
+        if (ch >= 'a' && ch <= 'z') {
+            arr ++;
+        }
+    }
+    for (int i = 0; i < 26; i++) {
+        if (arr[i] > 0) {
+            cout << char('a' + i) << ": " << arr [i] << endl;
+        }
+    }
     return 0;
 }*/
 
-
 //problem 7
-/*int main() {
-    int n;
-    cin >> n;
-    int arr[n];
-
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-
-    sort(arr, arr + n);
-
-    for (int i = 0; i < n; i++)
+/*void Print (int arr [], int size) {
+    sort(arr, arr + size);
+    int i = 0;
+    while (i < size) {
         cout << arr[i] << " ";
-
-    return 0;
+        i++;
+    }
+}
+int main () {
+    int arr[4] = {8, 1, 3, 3};
+    int size = sizeof(arr) / sizeof arr[0];
+    Print(arr, size);
 }*/
 
 
 //problem 8
 /*int main() {
+    int arr[6] = {0};
     srand(time(0));
-    int freq[6] = {0};
-
-    for (int i = 0; i < 10000; i++)
-        freq[rand() % 6]++;
-
-    for (int i = 0; i < 6; i++)
-        cout << i + 1 << ": " << freq[i] << endl;
-
+    for (int i = 0; i < 10000; i++) {
+    int roll = rand() % 6 + 1; 
+        arr[roll - 1]++; 
+    }
+    for (int i = 0; i < 6; i++) {
+        cout << (i + 1)<< arr[i] << endl;
+    }
     return 0;
 }*/
 
 //problem 9
 /*void reverseArray(int arr[], int start, int end) {
     while (start < end) {
-        swap(arr[start], arr[end]);
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
         start++;
         end--;
     }
 }
-
 int main() {
-    int n;
-    cin >> n;
-    int arr[n];
-
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-
-    reverseArray(arr, 0, n - 1);
-
-    for (int i = 0; i < n; i++)
+    int arr[3] = {1, 2, 3};
+    reverseArray(arr, 0, 2);
+    for (int i = 0; i < 3; i++) {
         cout << arr[i] << " ";
-
+    }
+    cout << endl;
     return 0;
 }*/
 
 
 //10 Problem
-/*bool isSorted(int arr[], int size) {
-    for (int i = 1; i < size; i++)
-        if (arr[i] < arr[i - 1])
-            return false;
-    return true;
-}
-
-int main() {
+/*int main() {
     int n;
     cin >> n;
-    int arr[n];
-
-    for (int i = 0; i < n; i++)
+    int arr[100]; 
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
-
-    cout << (isSorted(arr, n) ? "YES" : "NO");
+    }
+    bool sorted = true;
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < arr[i - 1]) {
+            sorted = false;
+            break;
+        }
+    }
+    if (sorted) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
     return 0;
 }*/
+
 
